fa0f10c95320648508eb9b1e054407048f57a857
Angelo Gazzola <angelo@nglgzz.com>
2020-04-27 17:40:10 +0200

    update electron test

--- packages/electron/src/electron.e2e.ts
+++ packages/electron/src/electron.e2e.ts
@@ -1,18 +1,26 @@
+import {INestApplication} from '@nestjs/common';
+import {SystemProvider} from '@relate/common';
 import {Test, TestingModule} from '@nestjs/testing';
-import {SystemProvider, SystemModule} from '@relate/common';
 
-describe('SampleService', () => {
-    let provider: SystemProvider;
+import {ElectronModule} from './electron.module';
+
+describe('ElectronModule', () => {
+    let app: INestApplication;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [SystemModule],
+            imports: [ElectronModule],
         }).compile();
 
-        provider = module.get(SystemProvider);
+        app = module.createNestApplication();
+        await app.init();
     });
 
-    it('should be defined', () => {
+    it('SystemProvider should be defined', () => {
+        expect(app).toBeDefined();
+
+        const provider = app.get(SystemProvider);
+
         expect(provider).toBeDefined();
     });
 });
