c6125e847beb49cf103c80ffb0421e3c7d8a6921
Hugo Bove <hugo.bove@neotechnology.com>
2020-05-06 09:53:09 +0200

    fixed run scripts

--- packages/cli/bin/run.js
+++ packages/cli/bin/run.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 /* eslint-disable */
+const path = require('path');
 
-require('../dist/index.js')
+require(path.join(__dirname, '../dist/index.js'))
     .bootstrap()
     .then(require('@oclif/command/flush'))
     .catch(require('@oclif/errors/handle'));
--- packages/web/bin/run.cmd
+++ packages/web/bin/run.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-set NODE_PATH=%~dp0..\node_modules
-
-node "%~dp0\run.js" %*
+node "%~dp0\run" %*
