5a66468c5c244aaddd0cfd9c44e98d6f48fc0636
Angelo Gazzola <angelo@nglgzz.com>
2020-05-06 15:28:19 +0200

    Create queries with apollo-link

--- packages/common/src/accounts/remote.account.ts
+++ packages/common/src/accounts/remote.account.ts
@@ -1,27 +1,36 @@
 import {IAuthToken} from 'tapestry';
-import got, {CancelableRequest} from 'got';
+import {execute, makePromise, ApolloLink, FetchResult, GraphQLRequest} from 'apollo-link';
+import {HttpLink} from 'apollo-link-http';
 
+import fetch from 'node-fetch';
+import gql from 'graphql-tag';
+
+import {FetchError, InvalidConfigError, NotAllowedError} from '../errors';
+import {IDbms, IDbmsVersion, AccountConfigModel} from '../models/account-config.model';
 import {AccountAbstract} from './account.abstract';
-import {NotAllowedError, InvalidConfigError, FetchError} from '../errors';
-import {IDbms, IDbmsVersion} from '../models/account-config.model';
 
 export class RemoteAccount extends AccountAbstract {
-    private graphql<T>(query: string, variables: {[key: string]: any}): CancelableRequest<T> {
+    private client: ApolloLink;
+
+    constructor(protected config: AccountConfigModel) {
+        super(config);
+
+        this.client = new HttpLink({
+            uri: this.config.relateURL,
+            // HttpLink wants a fetch implementation to make requests to a
+            // GraphQL API. It wants the browser version of it which has a
+            // few more options than the node version.
+            fetch: fetch as any,
+        });
+    }
+
+    private graphql(operation: GraphQLRequest): Promise<FetchResult<{[key: string]: any}>> {
         if (!this.config.relateURL) {
             throw new InvalidConfigError('Remote Accounts must specify a `relateURL`');
         }
 
         try {
-            return got<T>(this.config.relateURL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    query,
-                    variables,
-                }),
-            }).json();
+            return makePromise(execute(this.client, operation));
         } catch {
             throw new FetchError(`Failed to connect to ${this.config.relateURL}`);
         }
@@ -44,8 +53,8 @@ export class RemoteAccount extends AccountAbstract {
     }
 
     async getDbms(nameOrId: string): Promise<IDbms> {
-        const {data} = await this.graphql(
-            `query GetDbms ($accountId: String!, nameOrId: String!) {
+        const {data}: any = await this.graphql({
+            query: gql`query GetDbms ($accountId: String!, nameOrId: String!) {
                 getDbms(accountId: $accountId, nameOrId: $nameOrId) {
                     id,
                     name,
@@ -53,74 +62,101 @@ export class RemoteAccount extends AccountAbstract {
                     connectionUri
                 }
             }`,
-            {
+            variables: {
                 accountId: this.config.relateAccount,
                 nameOrId,
             },
-        );
+        });
 
         return data.getDbms;
     }
 
     async listDbmss(): Promise<IDbms[]> {
-        const {data} = await this.graphql(
-            `query ListDbmss ($accountId: String!) {
-                listDbmss(accountId: $accountId) {
-                    id,
-                    name,
-                    description,
-                    connectionUri
+        const {data}: any = await this.graphql({
+            query: gql`
+                query ListDbmss($accountId: String!) {
+                    listDbmss(accountId: $accountId) {
+                        id
+                        name
+                        description
+                        connectionUri
+                    }
                 }
-            }`,
-            {accountId: this.config.relateAccount},
-        );
+            `,
+            variables: {accountId: this.config.relateAccount},
+        });
 
         return data.listDbmss;
     }
 
     async startDbmss(namesOrIds: string[]): Promise<string[]> {
-        const {data} = await this.graphql(
-            `mutation StartDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
-                startDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
-            }`,
-            {
+        const {data}: any = await this.graphql({
+            query: gql`
+                mutation StartDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
+                    startDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
+                }
+            `,
+            variables: {
                 accountId: this.config.relateAccount,
                 namesOrIds,
             },
-        );
+        });
 
         return data.startDbmss;
     }
 
     async stopDbmss(namesOrIds: string[]): Promise<string[]> {
-        const {data} = await this.graphql(
-            `mutation StopDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
-                stopDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
-            }`,
-            {
+        const {data}: any = await this.graphql({
+            query: gql`
+                mutation StopDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
+                    stopDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
+                }
+            `,
+            variables: {
                 accountId: this.config.relateAccount,
                 namesOrIds,
             },
-        );
+        });
 
         return data.stopDbmss;
     }
 
     async statusDbmss(namesOrIds: string[]): Promise<string[]> {
-        const {data} = await this.graphql(
-            `query StatusDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
-                statusDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
-            }`,
-            {
+        const {data}: any = await this.graphql({
+            query: gql`
+                query StatusDBMSSs($accountId: String!, $namesOrIds: [String!]!) {
+                    statusDbmss(accountId: $accountId, dbmsIds: $namesOrIds)
+                }
+            `,
+            variables: {
                 accountId: this.config.relateAccount,
                 namesOrIds,
             },
-        );
+        });
 
-        return data.stopDbmss;
+        return data.statusDbmss;
     }
 
-    createAccessToken(_appId: string, _dbmsId: string, _authToken: IAuthToken): Promise<string> {
-        throw new NotAllowedError(`${RemoteAccount.name} does not support creating access tokens`);
+    async createAccessToken(appId: string, dbmsId: string, authToken: IAuthToken): Promise<string> {
+        const {data}: any = await this.graphql({
+            query: gql`
+                mutation AccessDBMS(
+                    $accountId: String!
+                    $dbmsName: String!
+                    $authToken: AuthTokenInput!
+                    $appId: String!
+                ) {
+                    createAccessToken(accountId: $accountId, dbmsId: $dbmsName, appId: $appId, authToken: $authToken)
+                }
+            `,
+            variables: {
+                accountId: this.config.relateAccount,
+                appId,
+                dbmsId,
+                authToken,
+            },
+        });
+
+        return data.createAccessToken;
     }
 }
