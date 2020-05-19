59fedfda3205124a1b5c3610e25f85b84fb6260f
Hugo Bove <hugo.bove@neotechnology.com>
2020-05-19 13:34:01 +0200

    spelling

--- packages/common/src/errors/error.abstract.ts
+++ packages/common/src/errors/error.abstract.ts
@@ -6,7 +6,7 @@ export abstract class ErrorAbstract extends Error {
     constructor(message: string, actions: string[] = []) {
         super(
             arrayHasItems(actions)
-                ? `${message}.\n\nSuggested Action(s);\n${join(
+                ? `${message}.\n\nSuggested Action(s):\n${join(
                       map(actions, (action) => `- ${action}`),
                       '\n',
                   )}`
