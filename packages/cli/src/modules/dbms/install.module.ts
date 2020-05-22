145708bb77a585e587d850409ca9311c6e304b03
Jas Kuner <jasraj.kuner@neotechnology.com>
2020-05-22 17:34:37 +0100

    remove online listing for cached dbms for dbms:install

--- packages/cli/src/modules/dbms/install.module.ts
+++ packages/cli/src/modules/dbms/install.module.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {OnApplicationBootstrap, Module, Inject} from '@nestjs/common';
 import {SystemModule, SystemProvider} from '@relate/common';
 import path from 'path';
@@ -28,7 +29,16 @@ export class InstallModule implements OnApplicationBootstrap {
 
         let {version} = flags;
         if (!version) {
-            const versions = await environment.listDbmsVersions();
+            let versions = await environment.listDbmsVersions();
+            versions = _.compact(_.map(versions, (version) => {
+                if (version.origin === 'cached') {
+                    return version
+                }
+                const cachedVersionExists = _.find(versions, (ver) => ver.origin === 'cached' && ver.version === version.version && ver.edition === version.edition);
+                if (!cachedVersionExists) {
+                    return version
+                }
+            }));
             version = await selectPrompt(
                 'Select a version to install',
                 versions.map((v) => ({
