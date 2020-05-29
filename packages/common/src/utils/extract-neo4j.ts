acaaa60a7320bc0442408298677239385aa02ee9
Jas Kuner <jasraj.kuner@neotechnology.com>
2020-05-29 19:08:53 +0100

    rely on file paths solely and any other properties from decompress

--- packages/common/src/utils/extract-neo4j.ts
+++ packages/common/src/utils/extract-neo4j.ts
@@ -1,28 +1,16 @@
 import _ from 'lodash';
-import fse from 'fs-extra';
-import decompress from 'decompress';
-import path from 'path';
 
-import {FileStructureError, DependencyError} from '../errors';
+import {FileStructureError} from '../errors';
 import {getDistributionInfo} from '../environments/local.environment/utils/dbms-versions';
 import {IDbmsVersion} from '../models';
+import { extract } from './extract';
 
 interface IExtractedArchive extends IDbmsVersion {
     extractedDistPath: string;
 }
 
 export const extractNeo4j = async (archivePath: string, outputDir: string): Promise<IExtractedArchive> => {
-    const outputFiles = await decompress(archivePath, outputDir);
-    // determine output dir filename from the shortest directory string path
-    const outputTopLevelDir = _.reduce(
-        _.filter(outputFiles, (file) => file.type === 'directory'),
-        (a, b) => (a.path.length <= b.path.length ? a : b),
-    );
-    if (!outputTopLevelDir) {
-        await Promise.all(_.map(outputFiles, (file) => fse.remove(path.join(outputDir, file.path))));
-        throw new FileStructureError(`Unexpected file structure after unpacking`);
-    }
-    const extractedDistPath = path.join(outputDir, outputTopLevelDir.path);
+    const extractedDistPath = await extract(archivePath, outputDir)
 
     // check if this is neo4j...
     try {
@@ -35,11 +23,6 @@ export const extractNeo4j = async (archivePath: string, outputDir: string): Prom
             extractedDistPath,
         };
     } catch (e) {
-        if (e.name === DependencyError.name) {
-            throw e;
-        }
-
-        await Promise.all(_.map(outputFiles, (file) => fse.remove(path.join(outputDir, file.path))));
         throw e;
     }
 };
