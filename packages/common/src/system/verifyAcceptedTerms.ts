f49a1ec06c45f9e22a9564db1e5b4c728593bef0
Angelo Gazzola <angelo@nglgzz.com>
2020-06-24 11:14:12 +0200

    Add util to programmatically accept Relate terms

--- packages/common/src/system/verifyAcceptedTerms.ts
+++ packages/common/src/system/verifyAcceptedTerms.ts
@@ -6,10 +6,24 @@ import {NotAllowedError} from '../errors';
 import {envPaths} from '../utils';
 
 const RELATE_ACCESS_CODE = 'r31473';
+const LICENSE_NAME = 'Relate EAP';
+const ACCEPTED_TERMS_PATH = path.join(envPaths().data, 'acceptedTerms');
+
+export const acceptTerms = async (app: string): Promise<void> => {
+    if (!app) {
+        throw Error(`Must provide a name for the application accepting the ${LICENSE_NAME} terms`);
+    }
+
+    const acceptedTerms = await fse.pathExists(ACCEPTED_TERMS_PATH);
+    if (acceptedTerms) {
+        return;
+    }
+
+    await fse.writeFile(ACCEPTED_TERMS_PATH, `${LICENSE_NAME} (${app})`);
+};
 
 export const verifyAcceptedTerms = async (): Promise<void> => {
-    const acceptedTermsPath = path.join(envPaths().data, 'acceptedTerms');
-    const acceptedTerms = await fse.pathExists(acceptedTermsPath);
+    const acceptedTerms = await fse.pathExists(ACCEPTED_TERMS_PATH);
 
     if (acceptedTerms) {
         return;
@@ -26,7 +40,7 @@ export const verifyAcceptedTerms = async (): Promise<void> => {
             'Enter the access code you received from applying at https://neo4j.relate.by/invite:',
             async (answer) => {
                 if (answer === RELATE_ACCESS_CODE) {
-                    await fse.writeFile(acceptedTermsPath, 'Relate EAP');
+                    await fse.writeFile(ACCEPTED_TERMS_PATH, LICENSE_NAME);
 
                     rl.close();
                     resolve();
