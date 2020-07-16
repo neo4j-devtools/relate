ed90772c9c15120bc7a84a4e94fd7f35d037ba69
Jas Kuner <jasraj.kuner@neotechnology.com>
2020-07-16 11:16:21 +0100

    fix extract test path issue

--- packages/common/src/utils/extract.test.ts
+++ packages/common/src/utils/extract.test.ts
@@ -3,6 +3,10 @@ import path from 'path';
 import {extract} from './extract';
 import {FileStructureError} from '../errors';
 
+const ARCHIVE_DIR_PATH = path.join('path', 'to', 'archive');
+const OUTPUT_DIR_PATH = path.join('path', 'to', 'outputDir');
+const EXTRACTED_ROOT_DIR = 'extractedRootDir';
+
 jest.mock('decompress', () => ({
     __esModule: true,
     default: jest
@@ -12,14 +16,14 @@ jest.mock('decompress', () => ({
                 data: Buffer.of(),
                 mode: 1,
                 mtime: '1234',
-                path: path.join('extractedRootDir', 'to', 'extracted', 'test-file'),
+                path: path.join(EXTRACTED_ROOT_DIR, 'to', 'extracted', 'test-file'),
                 type: 'file',
             },
             {
                 data: Buffer.of(),
                 mode: 1,
                 mtime: '4321',
-                path: path.join('another', 'extractedRootDir', 'to', 'extracted', 'test-file2'),
+                path: path.join('another', EXTRACTED_ROOT_DIR, 'to', 'extracted', 'test-file2'),
                 type: 'file',
             },
         ])
@@ -29,7 +33,7 @@ jest.mock('decompress', () => ({
                 data: Buffer.of(),
                 mode: 1,
                 mtime: '1234',
-                path: path.join('extractedRootDir', 'to', 'extracted', 'test-file'),
+                path: path.join(EXTRACTED_ROOT_DIR, 'to', 'extracted', 'test-file'),
                 type: 'file',
             },
         ]),
@@ -39,20 +43,20 @@ jest.mock('fs-extra');
 
 describe('extract', () => {
     test('more than 1 output root dir path', async () => {
-        await expect(extract('/path/to/archive', 'path/to/outputDir')).rejects.toThrow(
+        await expect(extract(ARCHIVE_DIR_PATH, OUTPUT_DIR_PATH)).rejects.toThrow(
             new FileStructureError('Unexpected file structure after unpacking'),
         );
     });
 
     test('no output output root dir path', async () => {
-        await expect(extract('/path/to/archive', 'path/to/outputDir')).rejects.toThrow(
+        await expect(extract(ARCHIVE_DIR_PATH, OUTPUT_DIR_PATH)).rejects.toThrow(
             new FileStructureError('Unexpected file structure after unpacking'),
         );
     });
 
     test('exactly 1 output root dir path', async () => {
-        await expect(extract('/path/to/archive', 'path/to/outputDir')).resolves.toBe(
-            path.join('extractedRootDir', 'to', 'extracted', 'test-file'),
+        await expect(extract(ARCHIVE_DIR_PATH, OUTPUT_DIR_PATH)).resolves.toBe(
+            path.join(OUTPUT_DIR_PATH, EXTRACTED_ROOT_DIR),
         );
     });
 });
