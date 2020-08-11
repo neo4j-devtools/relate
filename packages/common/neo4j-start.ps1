81a7fef621f67fd0fc8d51502a8c6db4b73d9116
Angelo Gazzola <angelo@nglgzz.com>
2020-08-11 15:29:29 +0200

    Fix encoding of DBMS logs on Windows
    When redirecting Powershell streams with `*>>` the content of the generated file is encoded with UTF-16LE.
    
    This is unexpected as it's different from the encoding neo4j uses when writing log files, and makes the logs unreadable when opened with the default UTF-8 encoding that many tools and languages use.

--- packages/common/neo4j-start.ps1
+++ packages/common/neo4j-start.ps1
@@ -3,9 +3,17 @@
 
 param (
     # Path to neo4j.ps1
-    [Parameter(Mandatory=$true)][string]$binPath,
+    [Parameter(Mandatory = $true)][string]$binPath,
     # Path to neo4j.log
-    [Parameter(Mandatory=$true)][string]$logsPath
+    [Parameter(Mandatory = $true)][string]$logsPath
 )
 
-Powershell -WindowStyle Hidden -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass -File "$binPath" console *>> "$logsPath"
+Powershell -NoProfile -NonInteractive -NoLogo `
+    -WindowStyle Hidden `
+    -ExecutionPolicy Bypass `
+    -File "$binPath" console  `
+    <# Redirect error, warning, and information streams into the success stream.
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_redirection?view=powershell-5.1 #> `
+    2>&1 3>&1 6>&1 | `
+    <# Append the success stream to the log file using the correct encoding #> `
+    Out-File -Append -Encoding utf8 "$logsPath"
