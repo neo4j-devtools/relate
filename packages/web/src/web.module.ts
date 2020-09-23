13038c9b57d3d0302c87b325561ec24a29c96f22
Hugo Bove <52443771+huboneo@users.noreply.github.com>
2020-09-23 18:28:08 +0200

    Add sofa-api and swagger-ui for generating REST API from graphql (#194)

--- packages/web/src/web.module.ts
+++ packages/web/src/web.module.ts
@@ -1,6 +1,10 @@
-import {Module} from '@nestjs/common';
-import {GraphQLModule} from '@nestjs/graphql';
+import {Inject, Module, OnModuleInit} from '@nestjs/common';
+import {GraphQLModule, GraphQLSchemaHost} from '@nestjs/graphql';
 import {SystemModule, EXTENSION_TYPES, loadExtensionsFor} from '@relate/common';
+import {HttpAdapterHost} from '@nestjs/core';
+import {Application} from 'express';
+import useSofa, {OpenAPI} from 'sofa-api';
+import swaggerUi from 'swagger-ui-express';
 
 import {ExtensionModule} from './entities/extension';
 import {DBModule} from './entities/db';
@@ -40,4 +44,40 @@ const dynamicModules = loadExtensionsFor(EXTENSION_TYPES.WEB);
         AuthModule,
     ],
 })
-export class WebModule {}
+export class WebModule implements OnModuleInit {
+    constructor(
+        @Inject(GraphQLSchemaHost) private readonly schemaHost: GraphQLSchemaHost,
+        @Inject(HttpAdapterHost) private readonly httpAdapterHost: HttpAdapterHost,
+    ) {}
+
+    onModuleInit(): void {
+        if (!this.httpAdapterHost) {
+            return;
+        }
+
+        const {httpAdapter} = this.httpAdapterHost;
+        const app: Application = httpAdapter.getInstance();
+        const {schema} = this.schemaHost;
+        const openApi = OpenAPI({
+            schema,
+            info: {
+                title: 'Relate REST API',
+                version: '1.0.0',
+            },
+        });
+
+        app.use(
+            '/api',
+            useSofa({
+                schema,
+                onRoute(info) {
+                    openApi.addRoute(info, {
+                        basePath: '/api',
+                    });
+                },
+            }),
+        );
+
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApi.get()));
+    }
+}
