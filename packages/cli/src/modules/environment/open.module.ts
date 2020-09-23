aa5b207fb4a1bc3cb86f311a2a9bcbad19b5ec63
Angelo Gazzola <angelo@nglgzz.com>
2020-09-23 13:07:50 +0200

    Remove interactive prompt from env:open

--- packages/cli/src/modules/environment/open.module.ts
+++ packages/cli/src/modules/environment/open.module.ts
@@ -3,7 +3,6 @@ import {SystemModule, SystemProvider} from '@relate/common';
 import cli from 'cli-ux';
 
 import OpenCommand from '../../commands/environment/open';
-import {selectEnvironmentPrompt} from '../../prompts';
 
 @Module({
     exports: [],
@@ -27,9 +26,7 @@ export class OpenModule implements OnApplicationBootstrap {
 
     async onApplicationBootstrap(): Promise<void> {
         const {args} = this.parsed;
-
-        const environmentId =
-            args.environment || (await selectEnvironmentPrompt('Choose an environment to update', this.systemProvider));
+        const {environment: environmentId} = args;
         const environment = await this.systemProvider.getEnvironment(environmentId);
 
         this.logOrOpen(environment.configPath);
