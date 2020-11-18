5b9f477b3ae272523a40f4250746187fb7988d90
Jas Kuner <jasraj.kuner@neotechnology.com>
2020-11-18 14:25:49 +0000

    add is-symlink check and update project and dbms link logic (#234)

--- packages/common/src/utils/files/index.ts
+++ packages/common/src/utils/files/index.ts
@@ -1,3 +1,4 @@
 export {PropertyEntries, readPropertiesFile} from './read-properties-file';
 export {writePropertiesFile} from './write-properties-file';
 export * from './map-file-to-model';
+export * from './is-symlink';
